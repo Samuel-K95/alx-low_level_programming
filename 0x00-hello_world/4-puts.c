f9bd3d574ac8e83acff00d2d2ba9fc1e1287db47
Samuel Keno <sgere195@gmail.com>
2023-07-13 12:42:36 -0700

    hello , puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
-
+/* main function */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0)
 }
