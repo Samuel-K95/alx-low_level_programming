9f4a20097eeabd84f5bad4299d3334e13b873d01
Samuel Keno <sgere195@gmail.com>
2023-07-13 13:29:41 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - driver code
+ * Description: prints size of data types
+ * Return: 0 to terminate the code
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int ln;
+	long long int lln;
+	float f;
+	printf("Size of a char %lu: ", (unsigned long)sizeof(c));
+	printf("Size of an int %lu: ", (unsigned int)sizeof(i));
+	printf("SIze of a long int %lu: ", (unsigned int)sizeof(ln));
+	printf("Size of a long long int %lu: ", (unsigned long)sizeof(lln));
+	printf("Size of a float %lu: ", (unsigned long)sizeof(f));
+	return (0);
+}
