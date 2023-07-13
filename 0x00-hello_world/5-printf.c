4080e73deb643812921591d4884324b353604c6d
Samuel Keno <sgere195@gmail.com>
2023-07-13 13:06:07 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - driver function
+ * Description: prints using printf
+ * Return: returns 0 to terminate code
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
