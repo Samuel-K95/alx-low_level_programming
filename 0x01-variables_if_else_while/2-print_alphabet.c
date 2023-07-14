df380ab7fe4b27449770ff97981e1c6b24b55398
Samuel Keno <sgere195@gmail.com>
2023-07-14 11:34:38 -0700

    lower alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - driver code
+ * Description: prints in lower format
+ * Return: 0 to terminate code
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
