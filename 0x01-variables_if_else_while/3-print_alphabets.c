187d5a1ac0d910afee0c0901e800a4790b0c5c1a
Samuel Keno <sgere195@gmail.com>
2023-07-14 11:53:20 -0700

    alphabet both

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - driver function
+ * Description: lower and upper alphabet
+ * Return: 0 to terminate program
+ */
+
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
+	return (0);
+}
