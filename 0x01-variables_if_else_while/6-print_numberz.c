ca3ec052c4f0bfff5efe24062470398b787649e5
Samuel Keno <sgere195@gmail.com>
2023-07-14 12:40:00 -0700

    0 ro 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - driver code
+ * Description: print numbers from 1 to 10
+ * Return: 0 to terminate code
+ */
+
+int main(void)
+{
+	int i, num, digit, reversed;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 0)
+		{
+			putchar('0');
+		}
+		else
+		{
+			num = i;
+			while (num > 0)
+			{
+				digit = num % 10;
+				reversed = reversed * 10 + digit;
+				num /= 10;
+			}
+			while (reversed > 0)
+			{
+				digit = reversed % 10;
+				putchar(digit + '0');
+				reversed /= 10;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
