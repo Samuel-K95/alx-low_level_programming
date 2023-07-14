b0c59e52b73a6b7d2d84a0f4bb9ef28673f26563
Samuel Keno <sgere195@gmail.com>
2023-07-14 12:14:13 -0700

    :numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - driver code
+ * Description: print numbers
+ * Return: 0 to terminate program
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
