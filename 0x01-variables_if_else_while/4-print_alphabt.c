6a35150bd87b9cd8cad7d85ec082b251b0ab78f2
Samuel Keno <sgere195@gmail.com>
2023-07-14 12:05:45 -0700

    aplphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - driver code
+ * Description: lower alphabets except q and e
+ * Return: 0 to terminate code
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+}
