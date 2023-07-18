e5bba624433464cc0f0dde30f924f1eefcf0f28d
Samuel Keno <sgere195@gmail.com>
2023-07-18 04:43:43 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - driver function
+ * Description: prints_putchar
+ * Return: 0 to terminate code
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
