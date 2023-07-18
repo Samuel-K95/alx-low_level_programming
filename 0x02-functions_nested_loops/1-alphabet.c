8c606a08acd114e33a2724d604671dab34941f26
Samuel Keno <sgere195@gmail.com>
2023-07-18 05:30:12 -0700

    print lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
