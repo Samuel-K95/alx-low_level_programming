5f819ffd1cd0ef673900d49b4c8cb52cae926e3a
Samuel Keno <sgere195@gmail.com>
2023-07-25 12:55:04 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - computes string length
+ * @s: string whose length to be measured
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
