665d05657d0d605af3ec072fab175d94575d4530
Samuel Keno <sgere195@gmail.com>
2023-07-25 02:54:26 -0700

    battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * reset_to_98 - resets integer to 98
+ * Description: resets a given integer to 98
+ * @n: integer to be changed
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
