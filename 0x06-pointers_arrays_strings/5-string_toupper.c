789cc8c16cf16c1fc2551693e679207180fdddf7
Samuel Keno <sgere195@gmail.com>
2023-07-29 14:19:57 -0700

    to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - changes to upper
+ * 
+ * @s: string to be changed to upper
+ * Return: string
+ */
+
+char *string_toupper(char *s)
+{
+	int pos;
+
+	pos = 0;
+
+	while (s[pos] != '\0')
+	{
+		if (s[pos] >= 97 && s[pos] <=122)
+		{
+			s[pos] = s[pos] - 32;
+		}
+
+		pos++;
+	}
+	return (s);
+}
