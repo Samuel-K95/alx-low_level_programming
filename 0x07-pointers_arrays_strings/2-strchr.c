4b740d0d618d5ccd394f1e56015dd6714ea39005
Samuel Keno <sgere195@gmail.com>
2023-07-31 15:18:01 -0700

    str chr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - search for the first occurence of apecific character
+ *
+ * @s: string in which the character is searched
+ * @c character to be searched
+ * Return: string
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+
+	return (0);
+}
