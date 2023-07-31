fed8cc6b627a2c9a3ca7d21ca8a73cba9187304a
Samuel Keno <sgere195@gmail.com>
2023-07-31 16:44:02 -0700

    substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_strchr(char *s, char c);
 int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
