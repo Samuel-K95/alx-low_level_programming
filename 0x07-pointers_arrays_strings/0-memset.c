2ca67370bcdd46f852541b15f0b4cb4ea547d6bf
Samuel Keno <sgere195@gmail.com>
2023-07-31 14:52:54 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,12 +12,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; n > 0; i++)
 	{
 		*s = b;
 		s++;
+		n--;
 	}
 
 	return (s);
