f665330338309dfba3fa214a27c7237b3cc2aa37
Samuel Keno <sgere195@gmail.com>
2023-08-05 04:34:50 -0700

    to infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - driver function
+ * Description: adds argv elements
+ * @argc: number of elements
+ * @argv: string of elements
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
