97985a4d7a8552b263829c2f3cdfe45cda04197c
Samuel Keno <sgere195@gmail.com>
2023-08-05 04:41:53 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,11 +14,12 @@ int main(int argc, char *argv[])
 {
 	int sum, i;
 
-	if (argc < 1)
+	if (argc == 1)
 	{
 		printf("%d\n", 0);
-		return (0);
 	}
+	else
+	{
 
 	for (i = 1; i < argc; i++)
 	{
@@ -32,7 +33,8 @@ int main(int argc, char *argv[])
 			return (1);
 		}
 	}
-
 	printf("%d\n", sum);
+	}
+
 	return (0);
 }
