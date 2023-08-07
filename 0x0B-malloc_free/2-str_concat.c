b18f84388d4d1ab8daa7183eed1679986dff1d47
Samuel Keno <sgere195@gmail.com>
2023-08-07 14:05:02 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int i, count, l1, l2;
-	char *dup;
+	char *dup, *string;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
@@ -25,12 +25,18 @@ char *str_concat(char *s1, char *s2)
 		i = 0;
 		l1 = 0;
 		l2 = 0;
+
+		if (s1 != NULL)
+		{
 		while (s1[i] != '\0')
 		{
 			count++;
 			i++;
 			l1++;
 		}
+		}
+		if (s2 != NULL)
+		{
 		i = 0;
 		while (s2[i] != '\0')
 		{
@@ -38,15 +44,35 @@ char *str_concat(char *s1, char *s2)
 			i++;
 			l2++;
 		}
-
-		count++;
-
+		}
+		if (s1 == NULL)
+			count = l2 + 1;
+		else if (s2 == NULL)
+			count = l1 + 1;
+		else
+			count = l1 + l2 + 1;
+			
 		dup = malloc(sizeof(char) * count);
+			
+		if (dup == NULL)
+			return (NULL);
+		if (s1 == NULL || s2 == NULL)
+		{
+			if (s1 == NULL)
+				string = s1;
+			else if (s2 == NULL)
+				string = s2;
 
-		for (i = 0; i < l1; i++)
-			dup[i] = s1[i];
-		for (i = 0; i < count; i++)
-			dup[l1 + i] = s2[i];
+			for (i = 0; i < count; i++)
+				dup[i] = string[i];
+		}
+		else
+		{
+			for (i = 0; i < l1; i++)
+				dup[i] = s1[i];
+			for (i = 0; i < count; i++)
+				dup[l1 + i] = s2[i];
+		}
 	}
 
 	return (dup);
