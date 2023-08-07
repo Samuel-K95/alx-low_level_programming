1f4f9dc78eead37feb60c145a464c867803538a8
Samuel Keno <sgere195@gmail.com>
2023-08-07 15:22:35 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,15 +17,21 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **)malloc(sizeof(int *) * width);
+	grid = malloc(sizeof(int *) * width);
 	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
-		grid[i] =(int *)malloc(sizeof(int) * height);
+		grid[i] = malloc(sizeof(int) * height);
 		if (grid[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(grid[i]);
+
+			free(grid);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < width; i++)
