aa7b712d9e543e9440a370029bc54a564cfa8215
Samuel Keno <sgere195@gmail.com>
2023-08-10 12:36:45 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,11 +13,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *mem;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	mem = malloc(size * nmemb);
 	if (mem == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		mem[i] = 0;
 	return (mem);
 }
