bb6b0b881d48425747840f7af968df16d4e01534
Samuel Keno <sgere195@gmail.com>
2023-08-10 06:56:28 -0700

    string_noconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,6 +32,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		concat[j] = s1[j];
 	for (j = 0; j < n; j++)
 		concat[i + j] = s2[j];
+	i = i + n + 1;
+	concat[i] = '\0';
+
 
 	return (concat);
 }
