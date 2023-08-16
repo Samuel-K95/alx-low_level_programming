b7183f4fdaaf000b4ea4b35b90f5ba54e55f9625
Samuel Keno <sgere195@gmail.com>
2023-08-16 02:21:12 -0700

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,5 +13,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int_index - search for number and return index
+ *
+ * @array: array to be checked
+ * @size: size of array
+ * @cmp: compared integer
+ * Return: integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
