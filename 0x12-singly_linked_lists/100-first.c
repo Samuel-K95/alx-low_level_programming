69e4105f5cd277f30398da5c2e48ee7b1be621d9
Samuel Keno <sgere195@gmail.com>
2023-08-24 12:01:35 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
+/**
+ * before_main - prints beofre main
+ *
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
