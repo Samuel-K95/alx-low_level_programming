bfdd34c9f63bb449a929e40a802461f0e2cfb783
Samuel Keno <sgere195@gmail.com>
2023-08-28 05:15:42 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free list and print nil
+ *
+ * @head: node to start fro
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *store;
+
+	if (*head == NULL)
+		return;
+	else
+	{
+		while ((*head) != NULL)
+		{
+			store= (*head)->next;
+			free(*head);
+			*head = store;
+		}
+
+		*head = NULL;
+	}
+}
