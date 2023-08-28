e5e747284484e5092d56b23c5738dae71097275a
Samuel Keno <sgere195@gmail.com>
2023-08-28 05:47:40 -0700

    6. Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - removes head
+ *
+ * @head: linked list
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *store;
+
+	if (*head == NULL)
+		return (0);
+	else
+	{
+		store= (*head)->next;
+		n = (*head)->n;
+		free(*head);
+		*head = store;
+	}
+
+	return (n);
+}
