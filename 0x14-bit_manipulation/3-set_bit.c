d7c9975c19643ad3e135cc8ad525ae4ff75c1726
Samuel Keno <sgere195@gmail.com>
2023-08-31 12:25:39 -0700

    11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * set_bit: sets bit at a position
+ * set_bit - sets bit at a position
  *
  * @n: number in string format
  * @index: position
  * Return: 1 or -1
  */
 
- int set_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	*n = ((1UL << index) | *n);
 
