ca43ab24393821b892da6b7bdb272dfe6664dd80
Samuel Keno <sgere195@gmail.com>
2023-09-04 14:10:45 -0700

    2. Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * append_text_to_file - appends text to file
+ *
+ * @filename: file to be appended on
+ * @text_content: text to be appended
+ * Return: int
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int curr, write_file, file_open;
+
+	if (filename == NULL)
+		return (-1);
+
+	curr = 0;
+
+	while (text_content[curr] != '\0')
+		curr++;
+	file_open = open(filename, O_WRONLY | O_APPEND);
+	if (file_open == -1)
+		return (-1);
+	write_file = write(file_open, text_content, curr);
+	if (write_file == -1)
+		return (-1);
+
+
+	return (1);
+}
