83e441644fe0aff67694d338022d4ec60f5d7803
Samuel Keno <sgere195@gmail.com>
2023-09-07 13:56:32 -0700

    undet the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,14 +24,10 @@ int create_file(const char *filename, char *text_content)
 		curr++;
 
 	file_ptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (file_ptr == -1)
-		return (-1);
 	file_write = write(file_ptr, text_content, curr);
 
-	if (file_write == -1)
+	if (file_ptr < 0 || file_write < 0)
 		return (-1);
-
 	close(file_ptr);
 
 	return (1);
