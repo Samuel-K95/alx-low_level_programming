8e7daa780e991879eb269546111dda81cecd80a1
Samuel Keno <sgere195@gmail.com>
2023-09-07 13:53:14 -0700

    under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	curr = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
