528f6a0e6f843aa58abb2e2832171d34d5d8e806
Samuel Keno <sgere195@gmail.com>
2023-09-23 01:11:03 -0700

    task2 file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
+	if (text_content != NULL)
+	{
 	while (text_content[curr] != '\0')
 		curr++;
+	}
 
 	file_ptr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	file_write = write(file_ptr, text_content, curr);
